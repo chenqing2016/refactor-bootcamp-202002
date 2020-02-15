b93c73d48e0daf37c49f5b0a1864998ecbf2dbd0
Qing Chen <qing.chen@thoughtworks.com>
2020-02-15 17:24:36 +0800

    [fibonacci]should-return-one-when-having-two-numbers

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -2,7 +2,7 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class Fibonacci {
     public int getFibonacci(int i) {
-        if (i==1){
+        if (i==1||i==2){
             return 1;
         }
         return 0;
